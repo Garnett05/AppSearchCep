f99adc2bc066626a94ff0b9e2efe6a2e02134098
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-03 20:12:18 -0300

    creating the cep search service

---
+++ ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConsultaCEP.Services;
+using ConsultaCEP.Services.Model;
+
+namespace ConsultaCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Button.Clicked += SearchCep;
+        }
+        private void SearchCep(object sender, EventArgs args)
+        {
+            string cep = Entry.Text.Trim();
+            Endereco end = ViaCEPService.BuscarEnderecoViaCEP(cep);
+
+            Result.Text = string.Format("Endereço: {0}, de {1}, {2}, {3}", end.Bairro, end.Localidade, end.Uf, end.Logradouro);
+        }
+        
+    }
+}
---
+++ ConsultaCEP/ConsultaCEP/Services/ViaCEPService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultaCEP.Services.Model;
+using Newtonsoft.Json;
+
+namespace ConsultaCEP.Services
+{
+    public class ViaCEPService
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
